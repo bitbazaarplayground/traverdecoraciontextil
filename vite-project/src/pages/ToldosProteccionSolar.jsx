dcb91f187f11e1075e01bb870b1cbf89567d62c9
Nico2-NT <nicolastraver93@gmail.com>
2025-12-18 20:01:15 +0000

    1

--- vite-project/src/pages/ToldosProteccionSolar.jsx
+++ vite-project/src/pages/ToldosProteccionSolar.jsx
@@ -7,7 +7,7 @@ import heroImg from "../assets/toldos/toldo2.jpg";
    Replace later with real assets
 ========================= */
 import toldoCofreImg from "../assets/toldos/cofre.jpg";
-import hosteleriaImg from "../assets/toldos/pergola.jpg";
+import hosteleriaImg from "../assets/toldos/hosteleria.jpg";
 
 import pergolaImg from "../assets/toldos/pergola.jpg";
 import toldoExtensibleImg from "../assets/toldos/toldo1.jpeg";
