6accf31bfe9faf57c5a09e6821bbb6fa43965ec7
Nico2-NT <nicolastraver93@gmail.com>
2026-02-16 13:00:10 +0000

    lazy loading Admin

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,9 +1,18 @@
 import react from "@vitejs/plugin-react";
+import { visualizer } from "rollup-plugin-visualizer";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({
+      open: true,
+      filename: "dist/stats.html",
+      gzipSize: true,
+      brotliSize: true,
+    }),
+  ],
   server: {
     host: true,
     port: 5173,
