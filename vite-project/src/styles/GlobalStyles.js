ee9ce17c39b27e7f4c29e73d5cab7a84a2ea0533
Nico2-NT <nicolastraver93@gmail.com>
2026-03-04 12:48:42 +0000

    Performance + img + slider

--- vite-project/src/styles/GlobalStyles.js
+++ vite-project/src/styles/GlobalStyles.js
@@ -1,29 +1,53 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
+@font-face {
+  font-family: "Playfair Display";
+  src: url("/fonts/PlayfairDisplay-SemiBold.woff2") format("woff2");
+  font-weight: 600;
+  font-style: normal;
+  font-display: swap;
+}
+
+@font-face {
+  font-family: "Playfair Display";
+  src: url("/fonts/PlayfairDisplay-SemiBoldItalic.woff2") format("woff2");
+  font-weight: 600;
+  font-style: italic;
+  font-display: swap;
+}
+
+/* existing font */
+
+@font-face {
+  font-family: "Cormorant Garamond";
+  src: url("/fonts/CormorantGaramond-300.woff2") format("woff2");
+  font-weight: 300;
+  font-style: normal;
+  font-display: swap;
+}
+
+@font-face {
+  font-family: "Cormorant Garamond";
+  src: url("/fonts/CormorantGaramond-300italic.woff2") format("woff2");
+  font-weight: 300;
+  font-style: italic;
+  font-display: swap;
+}
   * { margin: 0; padding: 0; box-sizing: border-box; }
 
   body { font-family: 'Helvetica Neue', Arial, sans-serif; }
 
-  h1, h2, h3, h4 { font-family: 'Playfair Display', serif; }
-
+  h1, h2, h3, h4 {
+    font-family: "Playfair Display", serif;
+    font-weight: 600;
+  }
+  em, i {
+    font-style: italic;
+  }
   p { line-height: 1.6; }
 
-  @font-face {
-    font-family: "Cormorant Garamond";
-    src: url("/fonts/CormorantGaramond-300.woff2") format("woff2");
-    font-weight: 300;
-    font-style: normal;
-    font-display: swap;
-  }
 
-  @font-face {
-    font-family: "Cormorant Garamond";
-    src: url("/fonts/CormorantGaramond-300italic.woff2") format("woff2");
-    font-weight: 300;
-    font-style: italic;
-    font-display: swap;
-  }
 `;
 
 export default GlobalStyles;
